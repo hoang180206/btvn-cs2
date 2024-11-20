55fef962c57bd89fece251932120ff36dc041ea8
hoang180206 <hoangnguyen29011802@gmail.com>
2024-11-20 21:10:19 +0700

    Add files via upload

---
+++ ex2-C.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+int main() {
+    // Khai bao va khoi tao hai bien so nguyen 
+    
+	// Bien so nguyen thu nhat
+	int number1 = 30; 
+    
+    // Bien so nguyen thu hai
+	int number2 = 12; 
+
+    // Tinh tong hai so va luu ket qua vao bien khac 
+    int total = number1 + number2;
+
+    // Ket qua 
+    printf("Tong: %d + %d = %d", number1, number2, total);
+
+    return 0;
+}
+
